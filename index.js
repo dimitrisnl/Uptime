83adc2a3efea43b80116c3e729ffed9512d2e6f6
Dimitrios Lytras <dnlytras@gmail.com>
2018-03-21 14:40:18 +0200

    update presentation

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
 // get something that is down from here http://www.isitdownrightnow.com/
-
 const axios = require("axios");
 const chalk = require("chalk");
-const logSymbols = require("log-symbols")
+const symbols = require("log-symbols");
 
 // Make them ENV variables
-
 const URLS = [
-  { url: "https://dimitrioslytras.com", isUp: true },
-  { url: "https://google.com", isUp: true },
-  { url: "https://cssmaid.netlify.com/", isUp: true },
-  { url: "http://www.000webhost.com", isUp: true},
-  { url: "http://www.sss.gov.ph", isUp: true },
-  { url: "http://www.mango.com", isUp: true },
-  { url: "http://www.leo.org", isUp: true }
+  { url: "https://dimitrioslytras.com", isUp: true, lastReachable: null },
+  { url: "https://google.com", isUp: true, lastReachable: null },
+  { url: "https://cssmaid.netlify.com/", isUp: true, lastReachable: null },
+  { url: "http://www.000webhost.com", isUp: true, lastReachable: null },
+  { url: "http://www.sss.gov.ph", isUp: true, lastReachable: null },
+  { url: "http://www.mango.com", isUp: true, lastReachable: null },
+  { url: "http://www.leo.org", isUp: true, lastReachable: null }
 ];
 
 const REQUEST_TIMEOUT = 5000;
 // const UPTIME_INTERVAL = 300000; // 5 minutes
-const UPTIME_INTERVAL = 100000; // 5 minutes
+const UPTIME_INTERVAL = 200000; // 5 minutes
 
-// const promises = URLS.map(url => axios.get(url, { timeout: REQUEST_TIMEOUT }));
+const checkForDowntime = async () => {
+  console.log("\n" + chalk.green("Checking status: ") + new Date() + "\n");
 
-const checkForDowntime = () => {
-  console.log('\n' + chalk.green('Checking status: ') + new Date() + '\n');
-  URLS.forEach(async entry => {
+  for (let entry of URLS) {
     try {
+      // Check if reachable
       const response = await axios.get(entry.url, { timeout: REQUEST_TIMEOUT });
-      console.log(` - ${response.config.url} ${logSymbols.success}`);
-      entry.isUp = true;
+      console.log(` ${symbols.success} ${response.config.url}`);
+      entry = { ...entry, isUp: true, lastReachable: new Date() };
     } catch (e) {
+      // Nope, no response
       entry.isUp = false;
-      console.log(` - ${e.config.url} ${logSymbols.error}`);
+      console.log(
+        ` ${symbols.error} ${e.config.url} - Last time up: ${chalk.underline(
+          entry.lastReachable || "N/A"
+        )}`
+      );
     }
-  });
+  }
 
-  // const reachable = URLS.filter(entry => entry.isUp).length
-  // console.log(reachable + '/' + URLS.length + ' are up')
+  const reachable = URLS.filter(entry => entry.isUp).length;
+  console.log(`\n ${symbols.info} ${reachable}/${URLS.length} are UP`);
   setTimeout(() => checkForDowntime(), UPTIME_INTERVAL);
-}
+};
 
 checkForDowntime();
