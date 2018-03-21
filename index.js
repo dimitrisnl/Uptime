2763169fd65961a4768fdfc63d18c0e1e531b1ec
Dimitrios Lytras <dnlytras@gmail.com>
2018-03-21 14:12:58 +0200

    show every website that is down

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
 // get something that is down from here http://www.isitdownrightnow.com/
 
-const axios = require('axios');
-const chalk = require('chalk');
+const axios = require("axios");
+const chalk = require("chalk");
+const logSymbols = require("log-symbols")
 
 // Make them ENV variables
 
 const URLS = [
-  'http://dimitrioslytras.com',
-  'http://google.com',
-  'https://cssmaid.netlify.com/',
-  'http://www.000webhost.com',
-  'http://www.sss.gov.ph',
+  { url: "https://dimitrioslytras.com", isUp: true },
+  { url: "https://google.com", isUp: true },
+  { url: "https://cssmaid.netlify.com/", isUp: true },
+  { url: "http://www.000webhost.com", isUp: true},
+  { url: "http://www.sss.gov.ph", isUp: true },
+  { url: "http://www.mango.com", isUp: true },
+  { url: "http://www.leo.org", isUp: true }
 ];
 
 const REQUEST_TIMEOUT = 5000;
 // const UPTIME_INTERVAL = 300000; // 5 minutes
 const UPTIME_INTERVAL = 100000; // 5 minutes
 
-
-const promises = URLS.map(url => axios.get(url, { timeout: REQUEST_TIMEOUT }));
-
-const handleSuccess = () => {
-  // flicker some green thingie
-  console.log(chalk.green('All good boss'));
-};
-
-const handleError = error => {
-  if (error.response) {
-    // Something other than 200
-    // probably ignore if not 500
-    console.log(chalk.red(error.response.status));
-  } else if (error.request) {
-    // No response
-    // raise the alarm
-    console.log(`${chalk.underline(error.config.url)} is ${chalk.red('DOWN')}`);
-  } else {
-    // Something happened while performing the request, ignore it
-  }
-};
+// const promises = URLS.map(url => axios.get(url, { timeout: REQUEST_TIMEOUT }));
 
 const checkForDowntime = () => {
-  axios
-    .all(promises)
-    .then(() => handleSuccess())
-    .catch(error => handleError(error))
-    .then(() => {
-      console.log('\n' + chalk.green('Last Checked: ') + new Date() + '\n\n');
-      setTimeout(() => checkForDowntime(), UPTIME_INTERVAL);
-    });
-};
+  console.log('\n' + chalk.green('Checking status: ') + new Date() + '\n');
+  URLS.forEach(async entry => {
+    try {
+      const response = await axios.get(entry.url, { timeout: REQUEST_TIMEOUT });
+      console.log(` - ${response.config.url} ${logSymbols.success}`);
+      entry.isUp = true;
+    } catch (e) {
+      entry.isUp = false;
+      console.log(` - ${e.config.url} ${logSymbols.error}`);
+    }
+  });
+
+  // const reachable = URLS.filter(entry => entry.isUp).length
+  // console.log(reachable + '/' + URLS.length + ' are up')
+  setTimeout(() => checkForDowntime(), UPTIME_INTERVAL);
+}
 
 checkForDowntime();
